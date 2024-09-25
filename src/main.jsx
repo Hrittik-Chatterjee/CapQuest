cc187489d14fe3ff39cbfc3791ce9a0cfeace6eb
Hrittik-Chatterjee <hrittik0chatterjee@gmail.com>
2024-09-26 03:09:23 +0600

    favicon added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
 
-// import { ToastContainer } from "react-toastify";
 import AuthProvider from "./authprovider/AuthProvider.jsx";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <AuthProvider>
         <RouterProvider router={router} />
-        {/* <ToastContainer /> */}
       </AuthProvider>
     </Provider>
   </React.StrictMode>
